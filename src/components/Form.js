c6c848716629c41dc1897f9eb18c27608f787c8b
persiopeo <persiopeo@hotmail.com>
2022-02-28 12:10:59 -0300

    Passando o Eslint

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,26 +6,34 @@ class Form extends React.Component {
     // eslint-disable-next-line max-len
     return (
       <form>
-        <label htmlFor="id-nome">Nome</label>
-        <input type="text" data-testid="name-input" id="id-nome" />
-        <label htmlFor="id-decricao" />
-        <input type="textarea" data-testid="description-input" id="id-descricao" />
-        <label htmlFor="id-attr1">Attr01</label>
-        <input type="number" data-testid="attr1-input" id="id-attr1" />
-        <label htmlFor="id-attr2">Attr02</label>
-        <input type="number" data-testid="attr2-input" id="id-attr2" />
-        <label htmlFor="id-attr3">Attr03</label>
-        <input type="number" data-testid="attr3-input" id="id-attr3" />
-        <labe htmlFor="id-imagem">Imagem</labe>
-        <input type="text" data-testid="image-input" id="id-imagem" />
-        <label>Raridade</label>
-        <select data-testid="rare-input">
-          <option value="normal">normal</option>
-          <option value="raro">raro</option>
-          <option value="muito raro">muito raro</option>
-        </select>
-        <input type="checkbox" data-testid="trunfo-input" id="id-check" />
-        <label>Super Trybe Trunfo</label>
+        <label htmlFor="name">
+          <input type="text" data-testid="name-input" id="name" />
+        </label>
+        <label htmlFor="desc">
+          <input type="textarea" data-testid="description-input" id="desc" />
+        </label>
+        <label htmlFor="id-attr1">
+          <input type="number" data-testid="attr1-input" id="id-attr1" />
+        </label>
+        <label htmlFor="id-attr2">
+          <input type="number" data-testid="attr2-input" id="id-attr2" />
+        </label>
+        <label htmlFor="id-attr3">
+          <input type="number" data-testid="attr3-input" id="id-attr3" />
+        </label>
+        <label htmlFor="id-imagem">
+          <input type="text" data-testid="image-input" id="id-imagem" />
+        </label>
+        <label htmlFor="id-raridade">
+          <select data-testid="rare-input" id="id-raridade">
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
+          </select>
+        </label>
+        <label htmlFor="id-check">
+          <input type="checkbox" data-testid="trunfo-input" id="id-check" />
+        </label>
         <button type="submit" data-testid="save-button">Salvar</button>
       </form>
     );
