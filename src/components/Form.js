7613f34323167fd6cabb3c05b84ce6252b61a3af
persiopeo <persiopeo@hotmail.com>
2022-02-28 22:57:18 -0300

    Atualizando lint

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Form extends React.Component {  
+class Form extends React.Component {
   render() {
     // requisito 2
     const {
